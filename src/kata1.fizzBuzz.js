e50927a851bf9f083c1c5df0dd00fe533b250c13
Nathan M <70609396+nathanmayall@users.noreply.github.com>
2021-03-09 21:30:38 +0000

    Add fizzbuzz function and test

--- src/kata1.fizzBuzz.js
+++ src/kata1.fizzBuzz.js
@@ -1,3 +1,14 @@
-const fizzBuzz = number => {};
+const fizzBuzz = (number) => {
+  if (number % 15 === 0) {
+    return `FizzBuzz, ${number}`;
+  }
+  if (number % 5 === 0) {
+    return `Buzz, ${number}`;
+  }
+  if (number % 3 === 0) {
+    return `Fizz, ${number}`;
+  }
+  return number.toString();
+};
 
 module.exports = fizzBuzz;
--- test/kata1.fizzBuzz.test.js
+++ test/kata1.fizzBuzz.test.js
@@ -1,11 +1,27 @@
 const { fizzBuzz } = require("../src");
 
 describe("fizzBuzz", () => {
-  test("returns Fizz when passed a multiple of 3", () => {});
+  test("returns Fizz when passed a multiple of 3", () => {
+    const result = fizzBuzz(12);
+    const expected = "Fizz, 12";
+    expect(result).toEqual(expected);
+  });
 
-  test("returns Buzz when passed a multiple of 5", () => {});
+  test("returns Buzz when passed a multiple of 5", () => {
+    const result = fizzBuzz(10);
+    const expected = "Buzz, 10";
+    expect(result).toEqual(expected);
+  });
 
-  test("returns FizzBuzz when passed a multiple 3 and 5", () => {});
+  test("returns FizzBuzz when passed a multiple 3 and 5", () => {
+    const result = fizzBuzz(30);
+    const expected = "FizzBuzz, 30";
+    expect(result).toEqual(expected);
+  });
 
-  test("returns the number when it isn't a multiple of 3 or 5", () => {});
+  test("returns the number when it isn't a multiple of 3 or 5", () => {
+    const result = fizzBuzz(8);
+    const expected = "8";
+    expect(result).toEqual(expected);
+  });
 });
