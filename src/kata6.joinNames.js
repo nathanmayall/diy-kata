31ae145fe76cc7754ad24fc93b64edce4c95bf0c
Nathan M <70609396+nathanmayall@users.noreply.github.com>
2021-03-09 21:31:49 +0000

    add joinNames function and test

--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -1,3 +1,17 @@
-const joinNames = namesObj => {};
+const joinNames = (namesObj) => {
+  let namesList = "";
+  namesObj.forEach((n, index) => {
+    if (index === 0) {
+      namesList += n.name;
+    }
+    if (index !== namesObj.length - 1 && index !== 0) {
+      namesList += `, ${n.name}`;
+    }
+    if (index === namesObj.length - 1) {
+      namesList += ` & ${n.name}`;
+    }
+  });
+  return namesList;
+};
 
 module.exports = joinNames;
--- test/kata6.joinNames.test.js
+++ test/kata6.joinNames.test.js
@@ -1,5 +1,27 @@
 const { joinNames } = require("../src");
 
 describe("joinNames", () => {
-  test("returns string of names, seperated by commas and an ampersand", () => {});
+  test("returns string of names, seperated by commas and an ampersand", () => {
+    const result = joinNames([
+      { name: "Bart" },
+      { name: "Lisa" },
+      { name: "Maggie" },
+    ]);
+    const expected = "Bart, Lisa & Maggie";
+    expect(result).toEqual(expected);
+  });
+  test("works for any length array", () => {
+    const result = joinNames([
+      { name: "Bart" },
+      { name: "Lisa" },
+      { name: "Maggie" },
+      { name: "Homer" },
+      { name: "Marge" },
+      { name: "Moe" },
+      { name: "Barney" },
+      { name: "Carl" },
+    ]);
+    const expected = "Bart, Lisa, Maggie, Homer, Marge, Moe, Barney & Carl";
+    expect(result).toEqual(expected);
+  });
 });
