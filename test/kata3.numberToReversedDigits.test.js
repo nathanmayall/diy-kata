43d85c516e7fff622077471cfe48bd0feb9dfac3
Nathan M <70609396+nathanmayall@users.noreply.github.com>
2021-03-09 21:31:00 +0000

    Add reversed number function and test

--- test/kata3.numberToReversedDigits.test.js
+++ test/kata3.numberToReversedDigits.test.js
@@ -1,5 +1,10 @@
 const { numberToReversedDigits } = require("../src");
 
 describe("numberToReversedDigits", () => {
-  test("returns a reversed array of the number's digits", () => {});
+  test("returns a reversed array of the number's digits", () => {
+    const array = [1, 2, 3, 4, 5];
+    const expected = [5, 4, 3, 2, 1];
+    const result = numberToReversedDigits(array);
+    expect(expected).toEqual(result);
+  });
 });
