1cfcffd268753316e395f714535385a590b13ed8
Nathan M <70609396+nathanmayall@users.noreply.github.com>
2021-03-09 21:31:11 +0000

    Add catdog years function and test

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,3 +1,21 @@
-const humanCatDogYears = number => {};
+const humanCatDogYears = (number) => {
+  if (number < 1) return [number, number, number];
+  const result = [number, 0, 0];
+  for (let i = 0; i <= number; i += 1) {
+    if (i === 1) {
+      result[1] += 15;
+      result[2] += 15;
+    }
+    if (i === 2) {
+      result[1] += 9;
+      result[2] += 9;
+    }
+    if (i > 2) {
+      result[1] += 4;
+      result[2] += 5;
+    }
+  }
+  return result;
+};
 
 module.exports = humanCatDogYears;
--- test/kata4.humanCatDogYears.test.js
+++ test/kata4.humanCatDogYears.test.js
@@ -1,3 +1,31 @@
 const { humanCatDogYears } = require("../src");
 
+describe("humanCatDogYears", () => {
+  it("returns all zeroes if number is zero", () => {
+    const result = humanCatDogYears(0);
+    const expected = [0, 0, 0];
+    expect(result).toEqual(expected);
+  });
+  it("returns 1, 15, 15 when number is 1", () => {
+    const result = humanCatDogYears(1);
+    const expected = [1, 15, 15];
+    expect(result).toEqual(expected);
+  });
+  it("returns 2, 24, 24 when number is 2", () => {
+    const result = humanCatDogYears(2);
+    const expected = [2, 24, 24];
+    expect(result).toEqual(expected);
+  });
+  it("returns 3, 28, 29 when number is 2", () => {
+    const result = humanCatDogYears(3);
+    const expected = [3, 28, 29];
+    expect(result).toEqual(expected);
+  });
+  it("returns 4, 32, 34 when number is 4", () => {
+    const result = humanCatDogYears(4);
+    const expected = [4, 32, 34];
+    expect(result).toEqual(expected);
+  });
+});
+
 // Look Ma, no handlebars!!!
