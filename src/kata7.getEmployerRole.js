3c241536ae6acb46ab9a7ecdaaa9d9d5cf8a14d1
Nathan M <70609396+nathanmayall@users.noreply.github.com>
2021-03-09 21:44:47 +0000

    add get role tests and function

--- src/kata7.getEmployerRole.js
+++ src/kata7.getEmployerRole.js
@@ -1,3 +1,7 @@
-const getEmployerRole = (employeeName, employees) => {};
+const getEmployerRole = (employeeName, employees) => {
+  const found = employees.find((e) => e.name === employeeName);
+  if (found) return found.role;
+  return "Not found!";
+};
 
 module.exports = getEmployerRole;
--- test/kata7.getEmployerRole.test.js
+++ test/kata7.getEmployerRole.test.js
@@ -1,5 +1,24 @@
 const { getEmployerRole } = require("../src");
 
 describe("getEmployerRole", () => {
-  test("returns the employee's role in the company", () => {});
+  test("returns the employee's role in the company", () => {
+    const data = [
+      { name: "Satti", role: "Developer" },
+      { name: "Jenny", role: "Sales Associate" },
+      { name: "Javid", role: "Human Recommended Reading Assistant" },
+    ];
+    const expected = "Developer";
+    const result = getEmployerRole("Satti", data);
+    expect(result).toEqual(expected);
+  });
+  test("returns not found if not found", () => {
+    const data = [
+      { name: "Satti", role: "Developer" },
+      { name: "Jenny", role: "Sales Associate" },
+      { name: "Javid", role: "Human Recommended Reading Assistant" },
+    ];
+    const expected = "Not found!";
+    const result = getEmployerRole("Nathan", data);
+    expect(result).toEqual(expected);
+  });
 });
