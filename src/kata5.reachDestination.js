f56c629c41c6d6cb3c86f33a07b4464a37bf48ee
Nathan M <70609396+nathanmayall@users.noreply.github.com>
2021-03-09 21:31:21 +0000

    Add reach destination function and test

--- src/kata5.reachDestination.js
+++ src/kata5.reachDestination.js
@@ -1,3 +1,4 @@
-const reachDestination = (distance, speed) => {};
+const reachDestination = (distance, speed) =>
+  `I should be there in ${Math.round((distance / speed) * 2) / 2} hours`;
 
 module.exports = reachDestination;
--- test/kata5.reachDestination.test.js
+++ test/kata5.reachDestination.test.js
@@ -1,5 +1,14 @@
 const { reachDestination } = require("../src");
 
 describe("reachDestination", () => {
-  test("returns string with estimated time of arrival", () => {});
+  test("returns string with estimated time of arrival", () => {
+    const result = reachDestination(100, 10);
+    const expected = "I should be there in 10 hours";
+    expect(result).toEqual(expected);
+  });
+  test("rounds to nearest 0.5 hour", () => {
+    const result = reachDestination(44, 10);
+    const expected = "I should be there in 4.5 hours";
+    expect(result).toEqual(expected);
+  });
 });
